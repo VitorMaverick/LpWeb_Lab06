de08f5840db15a92fc32206c912cca65b9c8641c
VitorMaverick <vitor.maverick@acad.ifma.edu.br>
2020-12-21 00:52:47 -0300

    Crud de livros funcionando

---
+++ app/repositorio/ProdutoRepository.js
@@ -0,0 +1,38 @@
+ 
+class ProdutoRepository {
+    
+    constructor(conexao) {
+       this._conexao = conexao;
+    }
+
+    porId(id, callback ) {
+        this._conexao.query(`select * from livro where id = ${id}`, callback);
+    }
+
+
+    todos(callback) {
+      this._conexao.query('select * from livro', callback);
+    }
+
+   
+    salva(livro, callback) {
+        console.log('ID ' + livro.id);
+
+        if ( (livro.hasOwnProperty('id')) && (livro.id > 0) ) {
+               this._conexao.query('update livro set ? where id = ' + livro.id, livro, callback);
+               console.log('executou update');
+
+        } else {
+            this._conexao.query('insert into livro set ?', livro, callback);
+            console.log('executou insert');
+
+        }    
+    }
+
+    remove(livro, callback) {
+        this._conexao.query('delete from livro where id = ' + livro.id, callback);
+    }
+
+} 
+
+module.exports = () => { return ProdutoRepository };
