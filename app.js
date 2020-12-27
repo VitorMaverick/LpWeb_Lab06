41631c158214964af1413e76429761aa1ba4f7a8
VitorMaverick <vitor.maverick@acad.ifma.edu.br>
2020-12-26 22:49:41 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 
 const app = require('./config/express')();
 
-// const rotasProdutos = require('./app/rotas/produtos')(app);
-// const rotaHome = require('./app/rotas/home')(app);
+
 
 const porta = 3000;
 
--- config/express.js
+++ config/express.js
@@ -1,12 +1,10 @@
 
 let express = require('express');
-let consign = require('consign');  // express-load
+let consign = require('consign'); 
 let bodyParser = require('body-parser');
 let expressValidator = require('express-validator');
 
 let app = express();
-
-//app.use(express.static('./app/public'));
 
 
 app.use('/css',express.static('./app/public/css'));
@@ -15,14 +13,12 @@ app.use('/images',express.static('./app/public/images'));
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
-//middleware (filtro)
-
 app.use(bodyParser.urlencoded({extended: true} ));
 app.use(expressValidator() );
-// req -> bodyParser -> validator -> função
 
 consign({cwd:'app'})
-   .include('rotas') // carrega as rotas automaticamente
+   .include('rotas') 
+   
    .then('infra')
    .then('repositorio')
    .into(app);
